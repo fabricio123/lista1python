ba5554f1d401edb906cc5af87be2ca7fe529d926
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-18 01:31:22 -0300

    Create questao8.py
    Programa que calcula o salario mensal a partir do valor pago por hora e do numero de hiras trabalhadas no mês referido.

---
+++ Questao8.py
@@ -0,0 +1,5 @@
+
+shora = float(input('quanto  você  ganha por hora? '))
+horas = float(input('quantas horas você  trabalha no mês'))
+salário = shora*horas
+print('o seu salario mensal é  de: ', salário)
