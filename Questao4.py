ee7b10ddf01557620753b0f7685a9dcc4ab4840d
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-18 01:20:29 -0300

    Create questao4.py
    Esse programa realiza a media aritmética de 4  notas e a imprime na tela.

---
+++ Questao4.py
@@ -0,0 +1,7 @@
+
+n1 = float(input('digite a primeira nota: '))
+n2 = float(input('digite a segunda nota: '))
+n3 = float(input('digite a terceira nota: '))
+n4 = float(input('digite a quarta nota: '))
+m = ((n1+n2+n3+n4)/4)
+print('a media é: {}'.format(m))
