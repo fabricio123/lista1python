a23239793292e132bcb5b0acfae4000f0f1e9f5a
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-18 01:37:13 -0300

    Create questao11.py
    Algoritmo que mostra na tela: O produto do dobro do primeiro com metade do segundo .
    Asoma do triplo do primeiro com o terceiro.
    O terceiro elevado ao cubo

---
+++ Questao11.py
@@ -0,0 +1,7 @@
+n1 = int(input('digte um número: '))
+n2 = int(input('digte um número: '))
+n3 = float(input('digte um número: '))
+prod = (n1*2)*(n2/2)
+soma = (n1*3)+n3
+cubo = n3**3
+print( prod, soma, cubo)
