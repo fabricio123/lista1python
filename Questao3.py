bbc892d9abd4a0eae970d8d00817d092f246c62e
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-18 01:17:28 -0300

    Create questao3.py
    O algoritmo le dois numero realiza a soma através de um operador aritmético que está  numa terceira variável  e imprime o resultado na tela.

---
+++ Questao3.py
@@ -0,0 +1,7 @@
+
+n1=int(input('digite o primeiro numero'))
+n2=int(input('digite o segundo numero'))
+soma=n1+n2
+#print ('a soma entre', n1, 'e', n2,'é:', soma)
+
+print('a soma entre {} e {} vale {}'.format(n1, n2, soma))
