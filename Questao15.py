4d8466bc4f7bacfc478484ae72b1c50942dc9530
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-21 09:56:02 -0300

    Alteração no arquivo

--- Questao15.py
+++ Questao15.py
@@ -3,9 +3,14 @@ ht = int(input('quantas horas vc trabalha no dia'))
 sbruto = slh * ht
 descontoimp = (sbruto*11)/100
 descinss = (sbruto*8)/100
-sliquido = sbruto - (descontoimp + descinss)
+sindicato = (sbruto*5)/100
+sliquido = sbruto - (descontoimp + descinss + sindicato)
+
 
 print('seu salario bruto é:R$', sbruto)
+print(' o desconto do sindicato é:', sindicato)
 print('o desconto do imposto de renda é:R$', descontoimp)
 print('o desconto do INSS é  de R$', descinss) 
 print('seu salario liquido é:R$', sliquido)
+
+
