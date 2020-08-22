9e3dfdc4989246e4b6aaee7e59b2a2eeea25d688
fabricio123 <50560978+fabricio123@users.noreply.github.com>
2020-08-22 08:36:57 -0300

    Create questao18.py

---
+++ Questao18.py
@@ -0,0 +1,6 @@
+tamanho = float(input('digite o tamanho do arquivo:'))
+velocidade = float(input('qual é a velocidade  da internet: '))
+result  = tamanho  /  velocidade * 60
+
+print (result, end =' ')
+print('minutos')
